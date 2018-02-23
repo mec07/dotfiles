e6bdb6865d6757bfde5ec86796456da176501b9d
Marc Coury <marccoury@ingresso.co.uk>
2018-02-23 12:03:05 +0000

    moved the delays in collecting mailboxes into offlineimap postsync

--- mutt/.mutt/collect_folders.fish
+++ mutt/.mutt/collect_folders.fish
@@ -2,12 +2,12 @@ function collect_folders
     set original_dir (pwd)
     cd ~/Maildir/$argv/
     set all_mailboxes (find . -type d | grep -v '\(cur\|new\|tmp\)$' | sed "s/\.\///g")
-    set mailbox_string ''
+    set mailbox_string 'mailboxes'
     for a_mailbox in $all_mailboxes
         if [ $a_mailbox != "." ]
             set -lx mailbox_contains_mail (contains_mail ~/Maildir/$argv/$a_mailbox)
             if [ $mailbox_contains_mail = "true" ]
-                set mailbox_string "$mailbox_string =\"$a_mailbox\""
+                set mailbox_string "$mailbox_string \"+$a_mailbox\""
             end
         end
     end
--- mutt/.mutt/offlineimap-postsync.sh
+++ mutt/.mutt/offlineimap-postsync.sh
@@ -1,3 +1,6 @@
 #!/usr/local/bin/bash
 
+/usr/local/bin/fish ~/.mutt/collect_folders.fish liveerrors > ~/.mutt/mailboxes.liveerrors
+/usr/local/bin/fish ~/.mutt/collect_folders.fish work > ~/.mutt/mailboxes.work
+
 notmuch new
